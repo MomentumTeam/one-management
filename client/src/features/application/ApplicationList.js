ea77b20d7bccc06c16a06a11eb7677ec4ae210b7
Sraya Avraham <sraya305@gmail.com>
2021-02-01 16:45:24 +0200

    refactor: card style

--- client/src/features/application/ApplicationList.js
+++ client/src/features/application/ApplicationList.js
@@ -6,16 +6,29 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Star from "../../components/Star";
 import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 300,
+    minHeight: 150,
+    background: 'linear-gradient( #e6e6e6 90%, teal 10%)'
+  },
+  media: {
+    height: 140,
+  },
+});
 
 function ApplicationList({ applicationList }) {
+  const classes = useStyles();
   return (
     <div style={{ marginTop: 20, padding: 30 }}>
       <Grid container spacing={4} justify="center">
         {applicationList.map((application) => (
           <Grid item key={application.id}>
-            <Card>
+            <Card className={classes.root}>
               <Star item={application}></Star>
-              <CardActionArea component={Link} to={`/application/${application.name}`}>
+              <CardActionArea className={classes.root} component={Link} to={`/application/${application.name}`}>
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="h2">
                     {application.name}
