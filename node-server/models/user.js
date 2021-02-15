3d4e5ec049670dd830793f21ea73a3209e9c7b67
Barak Steindl <steindl.barak@gmail.com>
2021-02-15 10:48:34 +0000

    node server and client removed

--- node-server/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  userId: String,
-  favorites: [String],
-  history: [String],
-});
-
-const User = mongoose.model("user", userSchema);
-module.exports = User;
